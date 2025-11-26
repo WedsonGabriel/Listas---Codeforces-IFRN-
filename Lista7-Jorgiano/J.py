498533d8636d2f950692b866aa64d634191e02ae
Wedson Gabriel <wedsongabriel163@gmail.com>
2025-11-26 11:15:28 -0300

    Lista 7

--- Lista7-Jorgiano/J.py
+++ Lista7-Jorgiano/J.py
@@ -1,1 +1,13 @@
-#Falta fazer
+tamanho = int(input())
+lista= []
+qtd_seq = 0
+
+for sequencia in range(tamanho):
+    numbers= int(input())
+    lista.append(numbers)
+
+for i in range(tamanho-1):
+    if lista[i] != lista[i+1]:
+        qtd_seq += 1
+
+print(qtd_seq + 1)
